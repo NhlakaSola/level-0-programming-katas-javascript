1eba093169050f7d5948be27a3ab51ae256a5a19
NhlakaSola <mzi2.sola@gmail.com>
2021-04-21 14:58:52 +0200

    implemented the changes

--- task-8.js
+++ task-8.js
@@ -2,26 +2,27 @@ function hourMinuteConvertor(int){
     if (int < 60) {
         let result = int % 60;
         if (result < 1) {
-            console.log(result + "minute");
+            console.log(result + " minute.");
         }else{
-            console.log(result + "minutes");
+            console.log(result + " minutes.");
         }
     } else{
         let result = parseInt(int / 60);
         let minutes = int % 60;
         if (result <= 1) {
             if (minutes <= 1) {
-                console.log(result + "hour," + minutes + "minute");
+                console.log(result + " hour, " + minutes + " minute.");
             }else{
-                console.log(result + "hour," + minutes + "minutes");
+                console.log(result + " hour, " + minutes + " minutes.");
             }
         }else{
             if (minutes <= 1) {
-                console.log(result + "hours," + minutes + "minute");
+                console.log(result + " hours, " + minutes + " minute.");
             }else{
-                console.log(result + "hours," + minutes + "minutes");
+                console.log(result + " hours, " + minutes + " minutes.");
             }
         }
 
     }
 }
+
