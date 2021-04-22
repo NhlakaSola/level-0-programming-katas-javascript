984822d006eaa53fe3e733597c5725ebeb132155
NhlakaSola <mzi2.sola@gmail.com>
2021-04-22 14:40:17 +0200

    fixed task-8.js

--- task-8.js
+++ task-8.js
@@ -8,10 +8,15 @@ function hourMinuteConvertor(int){
         }
     } else{
         let result = parseInt(int / 60);
-        let minutes = parseInt(int % 60);
+        let minutes = int % 60;
         if (result <= 1) {
-                minutes = 0;
+            if (int == 60) {
+                console.log(result + " hour, " + 0 + " minutes.");
+            }else if(minutes <= 1){
+                console.log(result + " hour, " + minutes + " minute.");
+            }else{
                 console.log(result + " hour, " + minutes + " minutes.");
+            }
         }else{
             if (minutes <= 1) {
                 console.log(result + " hours, " + minutes + " minute.");
