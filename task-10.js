a7af453540ac50531d8121931118d4b16d798457
NhlakaSola <mzi2.sola@gmail.com>
2021-04-22 14:32:55 +0200

    fixed task-10.js

--- task-10.js
+++ task-10.js
@@ -2,6 +2,7 @@ function commoners(firstString,secondString) {
     let a = firstString.toLowerCase().split('');
     let b = secondString.toLowerCase().split('');
     let results = [];
+    let checkingCharacter = "";
 
     for (let i = 0; i < a.length; i++) {
         for (let j = 0; j < b.length; j++) {
@@ -10,6 +11,14 @@ function commoners(firstString,secondString) {
             }
         }
     }
-    console.log(results.toString());
+    for (let i = 0; i < results.length; i++) {
+        checkingCharacter = results[i];
+        for (let j = i; j < results.length; j++) {
+            if (checkingCharacter == results[j+1]) {
+                results.splice(j+1,1);
+            }
+        }
+    }
+    console.log(`Common letters: ${results.toString()}`);
 }
 commoners("houses","computers");
